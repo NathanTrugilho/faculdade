7f6b2a33f09274b7710eccf62601c81c3d99da12
Nathan Trugilho Braga <trugicraft@gmail.com>
2024-01-27 13:48:09 -0300

    Mudanças no código

--- PIBIC/teste.py
+++ PIBIC/teste.py
@@ -4,12 +4,9 @@ x0 = []
 sigma1 = []
 sigma2 = []
 sigma3 = []
-G0 = []
+#G0 = []
 WA = []
-esq = [] # Frequência que resulta no -3dB que está à direita de x0
-dir = [] # Frequência que resulta no -3dB que está à esquerda de x0
-media = [] #(dir + esq)/2
-diferenca = [] #dir - esq
+delta = [] #(dir - esq)/2
 
 # Coleta os dados do arquivo ===============================================
 with open('dados.txt', 'r') as arquivo:
@@ -23,36 +20,42 @@ for linha in linhas:
     sigma1.append(float(valores[1]))
     sigma2.append(float(valores[2]))
     sigma3.append(float(valores[3]))
-    G0.append(float(valores[4]))
-    WA.append(float(valores[5]))
-    esq.append(float(valores[6]))
-    dir.append(float(valores[7]))
-    media.append(float(valores[8]))
-    diferenca.append(float(valores[9]))
-dados = [x0, sigma1, sigma2, sigma3, G0, WA, media, diferenca]
-#dados = [x0, sigma1, sigma2, sigma3, G0, WA, esq, dir, media, diferenca]
-dados_transpostos = list(map(list, zip(*dados)))
-# ==========================================================================
+    #G0.append(float(valores[4]))
+    WA.append(float(valores[4]))
+    delta.append(float(valores[5]))
 
-# Mudar aqui pra fazer funcionar
+dados = [x0, sigma1, sigma2, sigma3, WA]#, G0]
+dados_transpostos = list(map(list, zip(*dados)))
+
+# Configurações ====================================================================== 
 model = PySRRegressor(
-    niterations=40,  # < Increase me for better results
-    binary_operators=["+", "*", "-"],
+    model_selection="accuracy",
+    niterations=1000,  # < Increase me for better results
+    binary_operators=["+", "*", "-", "/"],
+    progress=False,
+    populations=50,
+    population_size=100,
+    maxsize=16,
+    delete_tempfiles=True,
+    # ^ Mostra apenas as equações finais, sem mostrar o progresso
     unary_operators=[
+        "sin",
         "cos",
         "exp",
-        "sin",
+        "log",
         "inv(x) = 1/x",
         # ^ Custom operator (julia syntax)
     ],
     extra_sympy_mappings={"inv": lambda x: 1 / x},
     # ^ Define operator for SymPy as well
+    warm_start=True,
+    turbo=True,
+    batching=True,
+    # ^ Faz com que o pysr rode a partir de um progresso já feito (a partir de um Hall_of_fame da vida)
     loss="loss(prediction, target) = (prediction - target)^2",
     # ^ Custom loss function (julia syntax)
 )
 
-model.fit(dados_transpostos,esq)
-
-#model.fit(X, y)
+model.fit(dados_transpostos, delta)
 
 print(model)
